13df754a6003785c80c76b188c2b1696c000857f
lisa_eriksson <lisa.c.eriksson@gmail.com>
2017-12-01 23:15:41 +0100

    Refactor step defs

--- features/step_definitions/create_user_steps.rb
+++ features/step_definitions/create_user_steps.rb
@@ -4,15 +4,6 @@ Given("The following user doesn't exist") do |table|
   end
 end
 
-When("I enter the {string} with {string}") do |field_name, value|
-  fill_in field_name, with: value
-end
-
-When("I enter {string} confirmation with {string}") do |field_name, value|
-  fill_in field_name, with: value
-end
-
 Then("I should be on the welcome page") do
-  binding.pry
-  expect(current_path).to eq welcome_index_path
+  expect(current_path).to eq root_path
 end
