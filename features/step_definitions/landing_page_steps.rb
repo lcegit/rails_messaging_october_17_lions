502e59b97f8d644df04b03eb4c4ac62ea88d1df1
aljoh <reachingalfred@gmail.com>
2017-12-01 13:09:02 +0100

    removed duplicate step definition

--- features/step_definitions/landing_page_steps.rb
+++ features/step_definitions/landing_page_steps.rb
@@ -2,10 +2,6 @@ Given("I visit the landing page") do
   visit root_path
 end
 
-Then("I should see {string}") do |text|
-  expect(page).to have_content text
-end
-
 When("I press the {string} button") do |link|
   click_link_or_button link
 end
